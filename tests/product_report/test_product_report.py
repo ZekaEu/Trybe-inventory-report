a2ff0a319404c130d68e238ebce872904cd7910f
ZekaEu <zeka.surf@hotmail.com>
2022-07-16 06:27:00 -0300

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        99,
+        "Magic: The Gathering",
+        "Wizards of The Coast - @Hasbro",
+        1993,
+        2019,
+        "M20-212/280",
+        "Sleeve/Shield"
+    )
+
+    assert produto.__repr__() == (
+        f"O produto {produto.nome_do_produto}"
+        f" fabricado em {produto.data_de_fabricacao}"
+        f" por {produto.nome_da_empresa} com validade"
+        f" até {produto.data_de_validade}"
+        f" precisa ser armazenado {produto.instrucoes_de_armazenamento}."
+    )
