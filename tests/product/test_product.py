ed2a261ce218a8784c6b781edd30e76629bcda36
ZekaEu <zeka.surf@hotmail.com>
2022-07-16 06:15:44 -0300

    Requisito 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        99,
+        "Magic: The Gathering",
+        "Wizards of The Coast - @Hasbro",
+        1993,
+        2019,
+        "M20-212/280",
+        "Sleeve/Shield"
+    )
+
+    assert produto.id == 99
+    assert produto.nome_do_produto == "Magic: The Gathering"
+    assert produto.nome_da_empresa == "Wizards of The Coast - @Hasbro"
+    assert produto.data_de_fabricacao == "1993"
+    assert produto.data_de_validade == "2019"
+    assert produto.numero_de_serie == "M20-212/280"
+    assert produto.instrucoes_de_armazenamento == "Sleeve/Shield"
